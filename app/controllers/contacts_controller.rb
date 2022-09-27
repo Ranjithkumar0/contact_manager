e93d47289d89eab16d3fa16fe947c01bd3f92f92
Ranjithkumar0 <ranjithkumar@sedintechnologies.com>
2022-09-27 18:10:44 +0530

    first

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,42 @@
+class ContactsController < ApplicationController
+  before_action :require_user_logged_in!
+  before_action :find_contact, only: [:edit, :update, :destroy]
+    def index
+      @contacts = Contact.all
+    end
+    def new
+     @contact = Contact.new
+    end
+    def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Successfully created new contact"
+      redirect_to contacts_path
+    else
+      render 'new'
+    end
+    end
+    def edit
+    end
+    def update
+      if @contact.update(contact_params)
+        flash[:success] = "Successfully Update contact"
+        redirect_to contacts_path
+      else
+        render 'edit'
+      end
+    end
+    def destroy
+      if @contact.destroy
+        flash[:success] = "Successfully Deleted contact"
+        redirect_to contacts_path
+      end
+    end
+    def find_contact
+      @contact = Contact.find(params[:id])
+    end
+    private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :company, :address ,:phone )
+    end
+  end
