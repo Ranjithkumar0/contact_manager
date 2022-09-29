a83498f5ca4f47aff9c150e507354863881d7b32
Ranjithkumar0 <ranjithkumar@sedintechnologies.com>
2022-09-29 14:03:05 +0530

    completed contact page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ApplicationRecord
     validates :name, :email, :phone, presence: true
+    validates :phone, presence: true, length: { is: 10 }
     validates_uniqueness_of :name, :email, :phone
     has_one_attached :image
 end
